7b55c39e9061f66130b82cccafb4252915ed90ed
pranitha.sirimalla <pranitha.sirimalla@gmail.com>
2025-10-15 16:28:25 -0500

    Search Component

--- foodApp/foodApp/src/App.jsx
+++ foodApp/foodApp/src/App.jsx
@@ -1,7 +1,9 @@
 import { useState } from "react";
-
+import Search from "./components/Search";
 function App() {
-  return <div className="App"></div>;
+  return <div className="App">
+    <Search />
+  </div>;
 }
 
 export default App;
---
+++ foodApp/foodApp/src/components/Search.jsx
@@ -0,0 +1,28 @@
+import React, { useEffect, useState } from 'react';
+export default function Search() {
+    const [query, setQuery] = useState("pizza");
+    const API_KEY="e5d16996706141e7b4d046c09680d416";
+    const URL="https://api.spoonacular.com/recipes/complexSearch";
+    useEffect(()=>{
+        async function fetchData(){
+            console.log(URL);
+            const res=await fetch(`${URL}?apiKey=${API_KEY}&query=${query}`,{method:'GET', headers: {
+                'Content-Type': 'application/json'
+            }});
+           
+            if (!res.ok) {
+                throw new Error(`HTTP error! status: ${res.status}`);
+            }
+
+            const data = await res.json();
+            console.log(data.results);
+        }
+    fetchData();
+    },[query]);
+    return (
+        <div>
+            <input type="text" placeholder="Search for food..."  value={query} onChange={(e)=>setQuery(e.target.value)}/>
+            
+        </div>
+    );
+}
